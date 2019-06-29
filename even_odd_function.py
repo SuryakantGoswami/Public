a0a72436e2ef486b068bb35105f05338a239362a
suryakant Goswami <suryakantgoswami92@gmail.com>
2019-06-30 00:49:54 +0530

    even odd program wwith function

---
+++ even_odd_function.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the Number: "))
+def even_odd(num):
+    if num%2==0:
+        print("Even",num)
+    else:
+        print("odd",num)
+
+for num in range(num):
+    even_odd(num)
